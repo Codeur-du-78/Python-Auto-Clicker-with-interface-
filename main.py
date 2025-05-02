b9359fe9c5ebcba1d5cfdabcaefb49b0ecbbc436
Codeur-du-78 <yahyasouleymane07@gmail.com>
2025-05-02 17:26:01 +0100

    Update and rename README.md to main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 This is a Python autoclicker that has an interface and allows you to choose the number of CPS you want, you can edit the code to make changes, or customize the code.
 import time
 code :
+
+
+
 import threading
 import tkinter as tk
 from pynput.mouse import Controller, Button
